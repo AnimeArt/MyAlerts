e8741b74e19dd32f03fce475f28715c7b0dbdc80
WildcardSearch <admin@rantcentralforums.com>
2013-09-21 12:09:33 -0500

    Working towrd #63
    Ported force_enable_alerts.php to the new ACP module.

--- admin/modules/myalerts/force_alerts.php
+++ admin/modules/myalerts/force_alerts.php
@@ -18,3 +18,51 @@ if (!defined("IN_MYBB")) {
 if (!isset($lang->myalerts)) {
     $lang->load('myalerts');
 }
+
+if (!function_exists('myalerts_is_installed') || myalerts_is_installed() != true) {
+	flash_message('MyAlerts is not installed!', 'error');
+	admin_redirect('index.php?module=myalerts');
+}
+
+// start over
+$db->delete_query('alert_setting_values');
+
+// get all alert types
+$alert_type_query = $db->simple_select('alert_settings');
+if ($db->num_rows($alert_type_query) == 0) {
+	flash_message('No Alert types detected!', 'error');
+	admin_redirect('index.php?module=myalerts');
+}
+
+$alert_type_count = 0;
+$alert_type_set = array();
+while ($alert_id = (int) $db->fetch_field($alert_type_query, 'id')) {
+	$alert_type_set[] = $alert_id;
+	++$alert_type_count;
+}
+
+// get all users
+$query = $db->simple_select('users', 'uid');
+if ($db->num_rows($query) == 0) {
+	flash_message('No users found to enable alerts for!', 'error');
+	admin_redirect('index.php?module=myalerts');
+}
+
+$user_count = 0;
+$settings = array();
+while ($uid = (int) $db->fetch_field($query, 'uid')) {
+	++$user_count;
+	$alert_type_user_set = array();
+
+	foreach ($alert_type_set as $id) {
+		$settings[] = array(
+			"user_id" => $uid,
+			"setting_id" => $id,
+			"value" => 1
+		);
+	}
+}
+$db->insert_query_multiple('alert_setting_values', $settings);
+
+flash_message("All alert types enabled for every user.<br /><br />Alert Types affected: {$alert_type_count}<br />Total Users affected: {$user_count}", 'success');
+admin_redirect('index.php?module=myalerts');
